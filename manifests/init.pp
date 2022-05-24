1fd089a4d9ec9c631f3ca5b2acb1bdaa42c8ba39
Lennart Betz <lennart.betz@netways.de>
2022-05-24 13:20:03 +0200

    add supported platforms and first unit test

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,4 @@
-# @summary A short summary of the purpose of this class
-#
-# A description of what this class does
+# @summary Manages a inotify based sync of one directory to several servers.
 #
 # @example
 #   class { 'isync':
--- spec/classes/isync_spec.rb
+++ spec/classes/isync_spec.rb
@@ -6,6 +6,14 @@ describe 'isync' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) do
+        {
+          src_dir: '/etc/puppetlabs/code/environments',
+          servers: [ '192.168.56.61', '192.168.56.62' ],
+          dst_dir: '/etc/puppetlabs/code/environments',
+          user: 'puppet',
+        }
+      end
 
       it { is_expected.to compile }
     end
